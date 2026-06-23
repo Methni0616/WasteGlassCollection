82865dffa25328c27a85d85cf9c38171a4f724de
Methni0616 <methnimanarandi7@gmail.com>
2026-06-24 03:47:23 +0530

    update

--- backend_dotnet/Program.cs
+++ backend_dotnet/Program.cs
@@ -3,18 +3,26 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddEndpointsApiExplorer();
-
 var app = builder.Build();
 
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Test route
+app.MapGet("/", () => "Waste Glass Collection API Running");
+
+// Controllers
 app.MapControllers();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
-
-app.Run($"http://0.0.0.0:{port}");
+app.Run();
